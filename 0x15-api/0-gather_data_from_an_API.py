1668713765f5ef7a604b47c1e674e0f4087204dd
jojostx <ikuskid7@gmail.com>
2023-04-25 01:33:54 -0700

    test corrections

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" queries JSONPlaceholder API to get information about employee """
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
-    userId = sys.argv[1]
-    res = requests.get(f"{url}users/{userId}")
+
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
     json_o = res.json()
-    name = json_o.get('name')
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
-    print(f"Employee {name} is done with tasks", end="")
-
-    res = requests.get(f"{url}todos?userId={userId}")
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
     tasks = res.json()
-
     l_task = []
     for task in tasks:
         if task.get('completed') is True:
@@ -24,3 +23,4 @@ if __name__ == "__main__":
     print("({}/{}):".format(len(l_task), len(tasks)))
     for task in l_task:
         print("\t {}".format(task.get("title")))
+
