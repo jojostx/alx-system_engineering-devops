79933ae2d60a1399f3265a5c8ad3f65d24745945
Ari Jay <ikuskid7@gmail.com>
2023-05-09 15:09:54 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes bug that causes apache error
 
 exec { 'repair incorrect import':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => [ '/bin', '/sbin' ]
 }
